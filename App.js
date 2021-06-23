868a8267022586269eaf60be4ffa3ee1bf2d715e
irizzo <isabelle965rizzo@gmail.com>
2021-06-22 21:33:36 -0300

    project's structure + first component

--- App.js
+++ App.js
@@ -2,10 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Componentes
+import Card from './src/components/Card.js';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{color: 'white'}}>Open up App.js to start working on your app!</Text>
+      <Card />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(80, 80, 80)',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/components/Card.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+/*
+  avatar
+  nome
+  nome de usuário
+  tempo
+  conteúdo
+  barrinha de funções
+*/
+
+export default function Card() {
+  return(
+    <View style={styles.container}>
+      <Text style={styles.text}>Nosso Card</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 100,
+    width: '100%',
+    backgroundColor: 'rgb(0, 0, 0)',
+  },
+
+  text: {
+    color: 'white'
+  }
+});
